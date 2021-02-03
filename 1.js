d7834447fee06b69d1b7a47bd0b55dfc00cc0f83
delena0702 <57484961+delena0702@users.noreply.github.com>
2021-02-03 23:49:40 +0900

    02.03

--- 1.js
+++ 1.js
@@ -1,13 +1,29 @@
 let ctx;
 const c_width = 500;
 const c_height = 700;
+const seeinert = 0.1;
+
+const basex2d = c_width - 110;
+const basey2d = 110;
+const rad2d = 100;
+const basex3d = c_width/2;
+const basey3d = c_height/2;
+
+const prad2d = 10;
+const prad3d = 10;
+const w2d = 15 * Math.cos(Math.PI / 4);
+const h2d = 15 * Math.sin(Math.PI / 4);
+const w3d = 50 * Math.cos(Math.PI / 6);
+const h3d = 50 * Math.sin(Math.PI / 6);
+
 const usekey = ['w', 'a', 's', 'd', ' '];
 
-const player_rad = 5;
+const player_rad = 10;
 
 let data;
 let width, height;
 let player;
+let see;
 let keystat = {};
 
 window.onload = () => {
@@ -37,14 +53,34 @@ function setContext(mode) {
 
     switch (mode) {
         case 1:
-            ctx.translate(c_width - 300, 300);
+            var tx = basex2d - (height + player.x - player.y) * w2d;
+            var ty = basey2d - (- width - height + player.x + player.y) * h2d;
+            
+            ctx.save();
+            ctx.lineWidth = 3;
+
+            ctx.beginPath();
+            ctx.arc(basex2d, basey2d, rad2d, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.clip();
+
+            ctx.fillStyle = "#444444aa";
+            ctx.fillRect(basex2d-rad2d, basey2d-rad2d, 2*rad2d, 2*rad2d);
+
+            ctx.translate(tx, ty);
+
             ctx.strokeStyle = "#000000";
             ctx.fillStyle = "#ffffff";
+            ctx.lineWidth = 1;
             break;
 
         case 2:
-            ctx.translate(10, c_height - 10);
-            ctx.strokeStyle = "#000000";
+            var tx = basex3d - (height + see.x - see.y) * w3d;
+            var ty = basey3d - ( - width - height + see.y + see.x - 2 * see.z) * h3d;
+
+            ctx.translate(tx, ty);
+
+            ctx.strokeStyle = "#444444";
             break;
 
         default:
@@ -75,51 +111,6 @@ function makeColor(num) {
 function show() {
     setContext(0);
     ctx.fillRect(0, 0, c_width, c_height);
-
-    const gap = 20;
-    const prad2d = 10;
-    const w2d = 20 * Math.cos(Math.PI / 4);
-    const h2d = 20 * Math.sin(Math.PI / 4);
-    const w3d = 20 * Math.cos(Math.PI / 6);
-    const h3d = 20 * Math.sin(Math.PI / 6);
-
-    // 평면
-    setContext(1);
-
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            if (data[i][j] == 0) continue;
-
-            ctx.fillStyle = makeColor(data[i][j] - player.z);
-
-            let x = height - i + j;
-            let y = - width - height + i + j;
-            ctx.beginPath();
-            ctx.moveTo((x) * w2d, (y) * h2d);
-            ctx.lineTo((x + 1) * w2d, (y + 1) * h2d);
-            ctx.lineTo((x) * w2d, (y + 2) * h2d);
-            ctx.lineTo((x - 1) * w2d, (y + 1) * h2d);
-            ctx.closePath();
-            ctx.fill();
-            ctx.stroke();
-        }
-    }
-
-    let px2d = height + player.x - player.y;
-    let py2d = - width - height + player.x + player.y;
-
-    ctx.beginPath();
-    ctx.fillStyle = "#00ffff";
-    ctx.moveTo(px2d * w2d, py2d * h2d);
-    ctx.lineTo(px2d * w2d + Math.cos(player.th - Math.PI / 2) * prad2d,
-        py2d * h2d + Math.sin(player.th - Math.PI / 2) * prad2d);
-    ctx.lineTo(px2d * w2d + Math.cos(player.th + Math.PI / 4) * prad2d,
-        py2d * h2d + Math.sin(player.th + Math.PI / 4) * prad2d);
-    ctx.lineTo(px2d * w2d + Math.cos(player.th + Math.PI) * prad2d,
-        py2d * h2d + Math.sin(player.th + Math.PI) * prad2d);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
 
     // 입체
     setContext(2);
@@ -167,19 +158,66 @@ function show() {
         }
     }
 
-    var px3d = height - player.y + player.x;
-    var py3d = - width - height + player.y + player.x - 2 * player.z;
+    let px3d = height - player.y + player.x;
+    let py3d = - width - height + player.y + player.x - 2 * player.z;
+    let ky3d = - width - height + player.y + player.x - 2 * data[0|player.y][0|player.x];
 
+    // 그림자
+    ctx.beginPath();
+    ctx.fillStyle = "#000000aa";
+    ctx.arc(px3d * w3d, ky3d * h3d, prad3d, 0, 2 * Math.PI);
+    ctx.fill();
+
+    // 플레이어
     ctx.beginPath();
     ctx.fillStyle = "#00ffff";
-    ctx.arc(px3d * w3d, py3d * h3d, player_rad, 0, 2 * Math.PI);
+    ctx.arc(px3d * w3d, py3d * h3d, prad3d, 0, 2 * Math.PI);
     ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(px3d * w3d, py3d * h3d);
-    ctx.lineTo(px3d * w3d + player_rad * Math.cos(player.th + Math.PI / 4),
-        py3d * h3d + player_rad * Math.sin(player.th + Math.PI / 4));
+    ctx.lineTo(px3d * w3d + prad3d * Math.cos(player.th + Math.PI / 4),
+        py3d * h3d + prad3d * Math.sin(player.th + Math.PI / 4));
     ctx.stroke();
+
+    // 평면
+    setContext(1);
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (data[i][j] == 0) continue;
+
+            ctx.fillStyle = makeColor(data[i][j] - player.z);
+
+            let x = height - i + j;
+            let y = - width - height + i + j;
+            ctx.beginPath();
+            ctx.moveTo((x) * w2d, (y) * h2d);
+            ctx.lineTo((x + 1) * w2d, (y + 1) * h2d);
+            ctx.lineTo((x) * w2d, (y + 2) * h2d);
+            ctx.lineTo((x - 1) * w2d, (y + 1) * h2d);
+            ctx.closePath();
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
+
+    let px2d = height + player.x - player.y;
+    let py2d = - width - height + player.x + player.y;
+
+    ctx.beginPath();
+    ctx.fillStyle = "#00ffff";
+    ctx.moveTo(px2d * w2d, py2d * h2d);
+    ctx.lineTo(px2d * w2d + Math.cos(player.th - Math.PI / 2) * prad2d,
+        py2d * h2d + Math.sin(player.th - Math.PI / 2) * prad2d);
+    ctx.lineTo(px2d * w2d + Math.cos(player.th + Math.PI / 4) * prad2d,
+        py2d * h2d + Math.sin(player.th + Math.PI / 4) * prad2d);
+    ctx.lineTo(px2d * w2d + Math.cos(player.th + Math.PI) * prad2d,
+        py2d * h2d + Math.sin(player.th + Math.PI) * prad2d);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
 }
 
 /**
@@ -187,7 +225,7 @@ function show() {
  */
 function initData() {
     data = [
-        [11, 10, 10, 9, 9, 9, 8, 8, 8, 7],
+        [0, 10, 10, 9, 9, 9, 8, 8, 8, 7],
         [10, 9, 9, 9, 8, 8, 8, 7, 7, 7],
         [9, 9, 8, 8, 8, 7, 7, 7, 6, 6],
         [9, 8, 8, 7, 7, 7, 6, 6, 6, 5],
@@ -199,6 +237,34 @@ function initData() {
         [7, 6, 6, 5, 4, 4, 3, 2, 2, 1],
     ];
 
+    /**
+    data = [
+        [99, 98, 89, 88, 71, 70, 45, 44, 11, 10],
+        [100, 97, 90, 87, 72, 69, 46, 43, 12, 9],
+        [95, 96, 91, 86, 73, 68, 47, 42, 13, 8],
+        [94, 93, 92, 85, 74, 67, 48, 41, 14, 7],
+        [81, 82, 83, 84, 75, 66, 49, 40, 15, 6],
+        [80, 79, 78, 77, 76, 65, 50, 39, 16, 5],
+        [59, 60, 61, 62, 63, 64, 51, 38, 17, 4],
+        [58, 57, 56, 55, 54, 53, 52, 37, 18, 3],
+        [29, 30, 31, 32, 33, 34, 35, 36, 19, 2],
+        [28, 27, 26, 25, 24, 23, 22, 21, 20, 1],
+    ];
+    /**/
+    data = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 3, 1],
+    ];
+    /**/
+
     width = data[0].length;
     height = data.length;
 
@@ -208,6 +274,11 @@ function initData() {
     player.z = data[height - 1][width - 1];
     player.th = - Math.PI / 4;
     player.zv = 0;
+
+    see = {};
+    see.x = player.x;
+    see.y = player.y;
+    see.z = player.z;
 }
 
 /**
@@ -226,7 +297,7 @@ function limiter(x, min, max) {
  * 매 프레임마다 실행되는 함수
  */
 function proc() {
-    const p_move = 0.1;
+    const p_move = 0.15;
     const xycheck = 1;
 
     let dx = 0, dy = 0;
@@ -277,13 +348,16 @@ function proc() {
     }
 
     if (player.z <= 0) {
-        console.log("die");
+        console.log("떨어짐");
         player.x = width - 0.5;
         player.y = height - 0.5;
         player.z = data[height - 1][width - 1];
         player.th = - Math.PI / 4;
         player.zv = 0;
     }
+
+    for (let i in see)
+        see[i] = (1-seeinert)*see[i] + seeinert*player[i];
 
     show();
 
